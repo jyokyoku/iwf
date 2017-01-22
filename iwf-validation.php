a675d14010cd97e38c06e49a669066eee55c5e89
m.ietomi <jyokyoku@gmail.com>
2017-01-22 20:53:28 +0900

    udpate IWF_Validation::run

--- iwf-validation.php
+++ iwf-validation.php
@@ -1001,7 +1001,8 @@ class IWF_Validation {
 	 */
 	public function validate_field( $field, array $data = null, $params = array() ) {
 		$params = wp_parse_args( $params, array(
-			'ignore_rules' => false
+			'ignore_rules'  => false,
+			'ignore_errors' => false,
 		) );
 
 		if ( empty( $data ) ) {
@@ -1040,7 +1041,9 @@ class IWF_Validation {
 			$result = self::callback( $value, $function, $args );
 
 			if ( $result === false ) {
+				if ( ! $params['ignore_errors'] ) {
 				return new IWF_Validation_Error( $this, $field, $rule, $value, $rule_params );
+				}
 
 			} else if ( $result !== true ) {
 				$value = $result;
